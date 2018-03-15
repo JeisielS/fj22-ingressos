51fc2cb9c0166b238f649a76a37b66dec496c93a
Jeisiel Silva <jeisiel_silva@hotmail.com>
2018-03-15 19:21:14 -0300

    Adiconado preço a Sala, filme e sesaao. Samando os Preços

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -1,5 +1,6 @@
 package br.com.caelum.ingresso.model;
 
+import java.math.BigDecimal;
 import java.time.LocalTime;
 
 import javax.persistence.Entity;
@@ -21,17 +22,27 @@ public class Sessao {
 	@ManyToOne
 	private Filme filme;
 
+	private BigDecimal preco;
+
 	/*
 	 * @deprected hibernate only
 	 */
 
 	public Sessao() {
 	}
-	public Sessao(LocalTime horario, Filme filme, Sala sala){
+
+	public Sessao(LocalTime horario, Filme filme, Sala sala) {
 		this.horario = horario;
 		this.filme = filme;
 		this.sala = sala;
+		this.preco = sala.getPreco().add(filme.getPreco());
 	}
+
+	// public Sessao(LocalTime horario, Filme filme, Sala sala){
+	// this.horario = horario;
+	// this.filme = filme;
+	// this.sala = sala;
+	// }
 
 	public LocalTime getHorarioTermino() {
 		return this.horario.plusMinutes(filme.getDuracao().toMinutes());
@@ -67,6 +78,14 @@ public class Sessao {
 
 	public void setFilme(Filme filme) {
 		this.filme = filme;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
 	}
 
 }
